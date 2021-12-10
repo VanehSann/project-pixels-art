30db1ac823e79c1b7e15f490de15f51218ac2e1e
VanehSann <vanessacbsantos22@gmail.com>
2021-12-10 15:49:07 -0300

    eu decidi apagar tudo e recomeçar apenas com o DOM

--- script.js
+++
@@ -1,8 +0,0 @@
-let selectedColor = document.querySelectorAll('.color');
-// inspirado em = https://stackoverflow.com/questions/65530318/add-remove-class-when-clicking-on-element-with-pure-javascript
-selectedColor.forEach(i => {
-  i.addEventListener('click', function() {
-    selectedColor.forEach(j => j.classList.remove('selected'));
-    this.classList.add('selected');
-  });
-});
