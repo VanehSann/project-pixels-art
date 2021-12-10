44038d0d9536a8d0452326e880bdfb84383d20b5
VanehSann <vanessacbsantos22@gmail.com>
2021-12-10 17:31:00 -0300

    requisito dois e tres + lint corrigido

---
+++ script.js
@@ -0,0 +1,12 @@
+// criando o #color-pallete
+const colorPalletePri = document.querySelector('.priCard');
+colorPalletePri.style.backgroundColor = 'rgb(0, 0, 0)';
+
+const colorPalleteSec = document.querySelector('.secCard');
+colorPalleteSec.style.backgroundColor = 'rgba(255, 0, 0, 1)';
+
+const colorPalleteTerc = document.querySelector('.tercCard');
+colorPalleteTerc.style.backgroundColor = 'rgba(255, 235, 59, 1)';
+
+const colorPalleteQuat = document.querySelector('.quatCard');
+colorPalleteQuat.style.backgroundColor = 'rgb(3, 169, 244, 1)';
