d38e10151ec6afe9e541199076e3c850a178178c
VanehSann <vanessacbsantos22@gmail.com>
2021-12-11 13:41:33 -0300

    organizando o lint - script

--- script.js
+++ script.js
@@ -12,29 +12,27 @@ const colorPalleteQuat = document.querySelector('.quatCard');
 colorPalleteQuat.style.backgroundColor = 'rgb(3, 169, 244, 1)';
 
 function linha() {
-for (let i = 0; i < 5; i ++){
-const cardPai = document.querySelector('#pixel-board');
-let cardFilho = document.createElement('div');
-cardPai.appendChild(cardFilho);
-cardFilho.style.backgroundColor = 'rgba(255, 255, 255, 255)';
-cardFilho.classList.add("pixel");
-console.log(cardPai);  
- console.log(cardFilho);  
+  for (let i = 0; i < 5; i += 1) {
+    const cardPai = document.querySelector('#pixel-board');
+    const cardFilho = document.createElement('div');
+    cardPai.appendChild(cardFilho);
+    cardFilho.style.backgroundColor = 'rgba(255, 255, 255, 255)';
+    cardFilho.classList.add('pixel');
+    console.log(cardPai);
+    console.log(cardFilho);
+  }
+}
 
-}
-}
 function coluna() {
- for (let i = 0; i < 5; i ++){
+  for (let i = 0; i < 5; i += 1) {
     const cardPaia = document.querySelector('#pixel-board');
-    let cardFilho = document.createElement('div');
+    const cardFilho = document.createElement('div');
     cardPaia.appendChild(cardFilho);
-    console.log(cardPaia)
-    }
-       
+    console.log(cardPaia);
+  }
 }
 
-for (let i = 0; i < 5; i ++){
-    coluna();
- linha();
+for (let i = 0; i < 5; i += 1) {
+  coluna();
+  linha();
 }
-    
