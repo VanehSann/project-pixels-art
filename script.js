26e3efff0422e9c2c59808ec6417e2d75a79e682
VanehSann <vanessacbsantos22@gmail.com>
2021-12-11 13:35:12 -0300

    requisito 4, 5 e 6

--- script.js
+++ script.js
@@ -10,3 +10,31 @@ colorPalleteTerc.style.backgroundColor = 'rgba(255, 235, 59, 1)';
 
 const colorPalleteQuat = document.querySelector('.quatCard');
 colorPalleteQuat.style.backgroundColor = 'rgb(3, 169, 244, 1)';
+
+function linha() {
+for (let i = 0; i < 5; i ++){
+const cardPai = document.querySelector('#pixel-board');
+let cardFilho = document.createElement('div');
+cardPai.appendChild(cardFilho);
+cardFilho.style.backgroundColor = 'rgba(255, 255, 255, 255)';
+cardFilho.classList.add("pixel");
+console.log(cardPai);  
+ console.log(cardFilho);  
+
+}
+}
+function coluna() {
+ for (let i = 0; i < 5; i ++){
+    const cardPaia = document.querySelector('#pixel-board');
+    let cardFilho = document.createElement('div');
+    cardPaia.appendChild(cardFilho);
+    console.log(cardPaia)
+    }
+       
+}
+
+for (let i = 0; i < 5; i ++){
+    coluna();
+ linha();
+}
+    
